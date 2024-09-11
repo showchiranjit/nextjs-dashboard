1a59c9e7e0b3d26b5303e302967f53b80ba95bbf
showch <chiranjit.04.me@gmail.com>
2024-09-11 22:32:02 +0530

    Initial commit from Create Next App

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Customers = () => {
+  return <div>Customer page</div>;
+};
+
+export default Customers;
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Invoices = () => {
+  return <div>Invoice page</div>;
+};
+
+export default Invoices;
